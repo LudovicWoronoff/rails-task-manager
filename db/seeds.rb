0370eeb553d17f3e6de6b1dc47b11656e50748cd
Ludovic Woronoff <ludovicworonoff@gmail.com>
2017-02-07 15:07:15 +0100

    modele task fini ainsi que toutes les fonction CRUD

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+t = Task.create!(name: "Coder", description: "Au Wagon")
+puts "Task #{t.name} created!"
+
+t = Task.create!(name: "Faire du sports", description: "courir mercredi soir + yoga")
+puts "Task #{t.name} created!"
+
+t = Task.create!(name: "Dejeuner", description: "trouver un endroit sympa rue Oberkampf")
+puts "Task #{t.name} created!"
